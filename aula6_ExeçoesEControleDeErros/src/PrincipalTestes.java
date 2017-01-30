bfb2bc21e6537dce798ea7b72309ef558b7c102b
uires deivide <uiresdeivide1@gmail.com>
2017-01-30 15:07:09 -0300

    Controlando erro: ArrayIndexOutOfBoundsException

--- aula6_ExeçoesEControleDeErros/src/PrincipalTestes.java
+++ aula6_ExeçoesEControleDeErros/src/PrincipalTestes.java
@@ -1,11 +1,8 @@
 public class PrincipalTestes {
 	public static void main(String[] args) {
 		System.out.println("Ínicio do teste Main: ");
-		try {
-			m1();
-		} catch (Exception e) {	
-			System.out.println("Problema  - -" + e);
-		}	
+		m1();
+
 		System.out.println("Fim do teste Main --");
 
 	}
@@ -13,18 +10,23 @@ public class PrincipalTestes {
 	private static void m1() {
 		System.out.println("Método nº -1");
 		m2();
-		System.out.println("Fim método nº -1");
 	}
 
 	private static void m2() {
 		System.out.println("Método nº -2");
 		int[] nums = new int[5];
 		for (int i = 0; i < 6; i++) {
-			nums[i] = i;
-			System.out.println("JJ- " + nums[i] * 2);
+			try{
+				nums[i] = i;
+				System.out.println("JJ- " + nums[i] * 2);
+			}catch(ArrayIndexOutOfBoundsException e){
+				System.out.println("Problema -- " + e);
+			}
+			
 		}
-		mF();
+		System.out.println("Fim método nº -1");
 		System.out.println("Fim do método nº -2");
+		mF();
 	}
 
 	private static void mF() {
